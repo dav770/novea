c0d472116a83ba48e340bf723a208677e34fee68
david cassuto <cassuto.david770@gmail.com>
2020-07-10 10:34:16 +0300

    Updates

---
+++ precache-manifest.ec06c51dd26a2f3f6b8bc70f6b92fad4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3ec9af4d1031d462e03a5f77db1165d",
+    "url": "/novea/index.html"
+  },
+  {
+    "revision": "b5841e057ca1c83a9fd2",
+    "url": "/novea/static/css/2.fd83e43b.chunk.css"
+  },
+  {
+    "revision": "da35bfec02afdcd9dd9e",
+    "url": "/novea/static/css/main.42591f24.chunk.css"
+  },
+  {
+    "revision": "b5841e057ca1c83a9fd2",
+    "url": "/novea/static/js/2.81191399.chunk.js"
+  },
+  {
+    "revision": "dd207babaeb913476be46155cc504ef5",
+    "url": "/novea/static/js/2.81191399.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da35bfec02afdcd9dd9e",
+    "url": "/novea/static/js/main.81cd7652.chunk.js"
+  },
+  {
+    "revision": "f491c5eb4b18ad15b2bf",
+    "url": "/novea/static/js/runtime-main.0de6f418.js"
+  }
+]);
